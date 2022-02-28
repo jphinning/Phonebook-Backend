9fc7a9cd391afe5a2eefce7f1d4af70434190767
Joao Hinning <jphinning@gmail.com>
2022-02-28 17:22:11 -0300

    db Connection Fix

--- build/config/database.js
+++ build/config/database.js
@@ -3,11 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ContactModel_1 = require("../models/ContactModel");
 const config = {
     type: "postgres",
-    host: process.env.POSTGRES_HOST || 'localhost',
-    port: Number(process.env.POSTGRES_PORT) || 5432,
-    username: process.env.POSTGRES_USER || 'postgres',
-    password: process.env.POSTGRES_PASSWORD || 'postgres',
-    database: process.env.POSTGRES_DB || 'phonebook',
+    url: process.env.DATABASE_URL,
+    // host: process.env.POSTGRES_HOST || 'localhost',
+    // port: Number(process.env.POSTGRES_PORT) || 5432,
+    // username: process.env.POSTGRES_USER || 'postgres',
+    // password: process.env.POSTGRES_PASSWORD || 'postgres',
+    // database: process.env.POSTGRES_DB || 'phonebook',
     entities: [ContactModel_1.ContactModel],
     synchronize: true,
 };
--- src/config/database.ts
+++ src/config/database.ts
@@ -3,11 +3,12 @@ import { ContactModel } from "../models/ContactModel";
 
 const config: ConnectionOptions = {
   type: "postgres",
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: Number(process.env.POSTGRES_PORT) || 5432,
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
-  database: process.env.POSTGRES_DB || 'phonebook',
+  url: process.env.DATABASE_URL,
+  // host: process.env.POSTGRES_HOST || 'localhost',
+  // port: Number(process.env.POSTGRES_PORT) || 5432,
+  // username: process.env.POSTGRES_USER || 'postgres',
+  // password: process.env.POSTGRES_PASSWORD || 'postgres',
+  // database: process.env.POSTGRES_DB || 'phonebook',
   entities: [ContactModel],
   synchronize: true,
 };
