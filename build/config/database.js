e52876725bf8f0be43594546c2feb6581058321c
Joao Hinning <jphinning@gmail.com>
2022-03-01 00:22:58 -0300

    db Connection Fix

--- build/config/database.js
+++ build/config/database.js
@@ -3,16 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ContactModel_1 = require("../models/ContactModel");
 const config = {
     type: "postgres",
-    url: process.env.DATABASE_URL || 'postgres://xtsoeetawuvysr:e6b430fbb2d40e46f0d7e9ac021ceffe87d6bc0ba6f0ddeabd95aca37192f38b@ec2-34-206-148-196.compute-1.amazonaws.com:5432/d7t4uk0l9l1dnl',
-    // host: process.env.POSTGRES_HOST || 'localhost',
-    // port: Number(process.env.POSTGRES_PORT) || 5432,
-    // username: process.env.POSTGRES_USER || 'postgres',
-    // password: process.env.POSTGRES_PASSWORD || 'postgres',
-    // database: process.env.POSTGRES_DB || 'phonebook',
+    // url: process.env.DATABASE_URL,
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: Number(process.env.POSTGRES_PORT) || 5432,
+    username: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
+    database: process.env.POSTGRES_DB || 'phonebook',
     entities: [ContactModel_1.ContactModel],
     synchronize: true,
     extra: {
-        ssl: true
+        ssl: true,
+        rejectUnauthorized: false
     }
 };
 exports.default = config;
